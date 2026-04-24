bc8e4a6ab7de42cc6aac3494d50c6ff7e221d0f5
Adittya Dey <adittya.dey@g.bracu.ac.bd>
2026-04-24 09:44:40 +0600

    added index links for country, university, sop

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Study Abroad System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Study Abroad Credit Transfer System</h1>
+</header>
+
+<main>
+    <h2>Features</h2>
+
+    <ul>
+        <li><a href="country.html">Country Filtration</a></li>
+        <li><a href="university.html">University Selection</a></li>
+        <li><a href="sop.html">SOP Database</a></li>
+    </ul>
+
+</main>
+
+</body>
+</html>
